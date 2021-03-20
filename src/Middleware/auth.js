505e5bcfc5f3aa938e98b804685b8ed79a418161
yash <yashkh2198@gmail.com>
2021-03-20 16:48:32 +0530

    auth Bug fix

--- src/Middleware/auth.js
+++ src/Middleware/auth.js
@@ -4,7 +4,7 @@ const User = require('../models/user')
 const auth = async (req, res, next) => {
     try {
         const token = req.header('Authorization').replace('Bearer ', '')
-        const decoded = jws.verify(token, 'thisIsAuthentication')
+        const decoded = jws.verify(token, process.env.AUTHENTICATION_KEY)
         const user = await User.findOne({ _id: decoded._id, 'tokens.token': token })
 
         if (!user) {
